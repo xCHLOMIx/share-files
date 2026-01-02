b0430a4ece2fa7ba3b2c00cc3c40b67575ea2f67
xCHLOMIx <gutabarwaa@gmail.com>
2026-01-02 13:52:08 +0200

    feat(auth): Add user sign in

--- models/user.model.js
+++ models/user.model.js
@@ -43,6 +43,38 @@ userSchema.statics.signup = async function (username, passcode) {
     }
 }
 
+userSchema.statics.signin = async function (username, passcode) {
+    let errors = {};
+
+    if (!username || username.trim() === '') {
+        errors.username = 'Username is required';
+    }
+
+    if (!passcode || passcode.trim() === '') {
+        errors.passcode = 'Passcode is required';
+    } else if (passcode.length !== 4) {
+        errors.passcode = 'Passcode must be 4 characters long';
+    }
+
+    if (Object.keys(errors).length > 0) throw new Error(JSON.stringify(errors));
+
+    const user = await this.findOne({ username });
+
+    if (!user) {
+        errors.username = 'No user with this username';
+        throw new Error(JSON.stringify(errors));
+    }
+
+    const passcodeMatch = await bcrypt.compare(passcode, user.passcode);
+
+    if (!passcodeMatch) {
+        errors.passcode = 'Incorrect passcode';
+        throw new Error(JSON.stringify(errors));
+    }
+
+    return user;
+};
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
